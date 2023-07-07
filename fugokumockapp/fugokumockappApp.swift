49720c7c68f282ea4ea4cacb2f850d4b1584adcb
Travis-ugo <okonicha.ugo@gmail.com>
2023-07-07 14:53:42 +0100

    Initial Commit

---
+++ fugokumockapp/fugokumockappApp.swift
@@ -0,0 +1,17 @@
+//
+//  fugokumockappApp.swift
+//  fugokumockapp
+//
+//  Created by Travis Okonicha on 07/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct fugokumockappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
